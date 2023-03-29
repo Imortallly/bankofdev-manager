38af37d3f342fe56234e85f246d67302e25797af
João Vitor Gomes Nunes <joaovitor.jgn@gmail.com>
2023-03-28 22:42:12 -0300

    Ajustado depois de 5 dias sem mexer o box-info

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,14 +10,17 @@ function Home() {
             <div className='space-header'></div>
             <section className='welcome-screen'>
                 <div className='box-info'>
-                    <div>
+                    <div className='new'>
                         <span><img src={lanterna} /> NEW: Now you can access these features yourself from home! <a href='#'>Try Bank of Dev mobile!</a></span>
                     </div>
                     <span className='logo-show'><img src={icon} />Bank of Dev</span>
                     <p><spa className='title-show'>Your bank with thousands of options is here.</spa><br></br>
                     <h3>Join 12 million others taking control of their finances.</h3>
                     </p>
-                    <input type='text'></input>
+                    <div className='email'>
+                    <input type='text' placeholder='Enter your email'></input>
+                    <button className='score-button'>See my Score</button>
+                    </div>
                 </div>
                 <div className='image-show'>
 
